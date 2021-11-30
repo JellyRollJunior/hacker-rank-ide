32177c3cc2fc6ea200c107667566972e4f61ef1f
Brandon Lin <brandonlin9716@gmail.com>
2021-11-30 12:00:15 -0800

    Understood lonelyInteger problem solution.
    This took way too long. It's been a long time since I've used bitwise operators. Time to study up on it.

--- src/Main.java
+++ src/Main.java
@@ -15,6 +15,12 @@ public class Main {
             O(N) , O(1) space solution : I need to focus more on mathematics rather than relying on
             prebuilt functions
 
+            Property: if a binary number A is XOR by B twice, the result is A;
+                      in short: XOR is commutative and associative
+
+            Since we know the lonely integer only appears once and repeated elements appear twice,
+            Every value will cancel itself out except the lonely integer
+
             int val = 0;
             for (int num : a) {
                 val = val ^ num; // ^ is XOR operator
