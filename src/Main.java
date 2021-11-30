7bd1530da92c2d9a6ddd5f5d7bed66a28fea490c
Brandon Lin <brandonlin9716@gmail.com>
2021-11-30 10:53:02 -0800

    Completed timeConversion question.

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,30 @@
-import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
+        String time = "12:05:45PM";
+        System.out.println(timeConversion(time));
+    }
 
+    /*
+     * Given a time in -hour AM/PM format, convert it to military (24-hour) time.
+     * Note: - 12:00:00AM on a 12-hour clock is 00:00:00 on a 24-hour clock.
+     * - 12:00:00PM on a 12-hour clock is 12:00:00 on a 24-hour clock.
+     */
+    public static String timeConversion(String s) {
+        String militaryTime = s.replaceAll("[A-Za-z]", "");
+        String hour = s.substring(0, 2);
+        String amPm = s.replaceAll("[^A-Za-z]", "");
+
+        if (amPm.equals("AM") && hour.equals("12")) {
+            militaryTime = "00" + militaryTime.substring(2);
+        } else if (amPm.equals("PM") && !hour.equals("12")) {
+            int militaryHour = Integer.parseInt(hour) + 12;
+            militaryTime = "" + militaryHour + militaryTime.substring(2);
+        }
+
+        return militaryTime;
     }
 
     /*
