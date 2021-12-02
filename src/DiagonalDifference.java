e4c315906e99e96e110419db16a18b971e5a9b9d
Brandon Lin <brandonlin9716@gmail.com>
2021-12-02 11:58:31 -0800

    Optimal solution for DiagonalDifference notes.

--- src/DiagonalDifference.java
+++ src/DiagonalDifference.java
@@ -1,11 +1,24 @@
 import java.util.List;
 
 public class DiagonalDifference {
+
     /*
      *     Given a square matrix, calculate the absolute difference between the sums of its diagonals.
      */
-
     public static int diagonalDifference(List<List<Integer>> arr) {
+        /*
+         * Optimal N vs 2N runtime
+         * Reflection: again, I need to remember to use mathematical principles to shorten my code
+         *
+         * The cleanest solution actually doesn't track both sums
+         *
+         * int difference = 0;
+         * for (int i = 0; i < arrSize; i++) {
+         *     difference += arr.get(i).get(i) - arr.get(i).get(arrSize - 1 - i);
+         * }
+         *
+         * return Math.abs(difference)
+         */
         int arrSize = arr.size();
 
         // compute LR diagonal
