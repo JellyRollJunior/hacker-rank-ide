993c9b90e928d79b706f3dc5d84355ccffd75879
Brandon Lin <brandonlin9716@gmail.com>
2021-12-02 17:39:47 -0800

    Study one line solution to Pangrams.

--- src/Pangrams.java
+++ src/Pangrams.java
@@ -5,9 +5,21 @@ public class Pangrams {
      * Ignore case. Return either pangram or not pangram as appropriate.
      */
     public static String pangrams(String s) {
+        /*
+         * Reflection: I need to read the prompt more carefully
+         * If I read the prompt more clearly, I would have seen that the set of chars are {A-Z, a-z, "", " "}
+         * I could just replace the " " and done toLower for fewer comparisons
+         *
+         * OR
+         *
+         * I could do this a one line solution from the discussion:
+         * return s.toLowerCase().replace(" ", "").chars().distinct().count() == 26 ? "pangram" : "not pangram";
+         *
+         * very succinct! but also a little unclear at a glance in my opinion
+         * I would have separated it into a couple lines, but I know u gotta flex the one line solution
+         */
         String isPangram = "pangram";
         int[] alphabetCount = new int[26];
-
         int A = 65;
         int Z = 90;
         int a = 97;
