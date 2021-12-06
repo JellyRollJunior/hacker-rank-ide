4604b761e1579455af3e2a2024861f6b2c7d6cb7
Brandon Lin <brandonlin9716@gmail.com>
2021-12-05 16:54:33 -0800

    Completed my solution to SalesByMatch.java.

---
+++ src/SalesByMatch.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class SalesByMatch {
+
+    /*
+     * There is a large pile of socks that must be paired by color.
+     * Given an array of integers representing the color of each sock,
+     * determine how many pairs of socks with matching colors there are.
+     */
+    public static int sockMerchant(int n, List<Integer> ar) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        List<Integer> distinctColors = new ArrayList<>();
+
+        for (int color : ar) {
+            int timesAppeared = map.getOrDefault(color, 0);
+
+            if (timesAppeared == 0) {
+                distinctColors.add(color);
+            }
+
+            map.put(color, ++timesAppeared);
+        }
+
+        int totalMatches = 0;
+        for (int color : distinctColors) {
+            int timesAppeared = map.getOrDefault(color, 0);
+
+            totalMatches += timesAppeared / 2;
+        }
+
+        return totalMatches;
+    }
+
+    public static void main(String[] args) {
+        List<Integer> socks = new ArrayList<>();
+        socks.add(1);
+        socks.add(1);
+        socks.add(2);
+        socks.add(3);
+        socks.add(3);
+        socks.add(1);
+
+        System.out.println(sockMerchant(socks.size(), socks));
+    }
+}
