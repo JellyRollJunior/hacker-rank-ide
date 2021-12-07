da5971d048dc2ba6f489e653a12239a8f126e391
Brandon Lin <brandonlin9716@gmail.com>
2021-12-06 18:28:57 -0800

    Completed my solution for MaxMin.java.

---
+++ src/MaxMin.java
@@ -0,0 +1,25 @@
+import java.util.Collections;
+import java.util.List;
+
+public class MaxMin {
+
+    /*
+     * You will be given a list of integers arr, and a single integer k. You must create an array of length k
+     * from elements of arr such that its unfairness is minimized. Call that array arr' .
+     * Unfairness of an array is calculated as max(arr') - min(arr')
+     */
+    public static int maxMin(int k, List<Integer> arr) {
+        Collections.sort(arr);
+
+        int endOffset = k - 1;
+        int unfairness = arr.get(endOffset) - arr.get(0);
+        for (int i = 1; i + (endOffset) < arr.size(); i++) {
+            int nextUnfairness = arr.get(i + endOffset) - arr.get(i);
+
+            if (nextUnfairness < unfairness) {
+                unfairness = nextUnfairness;
+            }
+        }
+        return unfairness;
+    }
+}
