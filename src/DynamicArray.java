66b0a89f7c8c723c8cbd6ed60a71eed55a1ff055
Brandon Lin <brandonlin9716@gmail.com>
2021-12-07 20:27:22 -0800

    Finished my Reflection to DynamicArray.java.

--- src/DynamicArray.java
+++ src/DynamicArray.java
@@ -8,6 +8,14 @@ public class DynamicArray {
      * https://www.hackerrank.com/challenges/dynamic-array/problem
      */
     public static List<Integer> dynamicArray(int n, List<List<Integer>> queries) {
+        /*
+         * Reflection: Honestly, kind of a stupid question. The hard part is understanding what the problem wants you
+         *             to do not the complexity or the problem itself.
+         *
+         * One thing I could have improved was renaming the query.get(index) and making them into actual variables
+         * to improve clarity and readability. Other than that I don't think theres anything else to say
+         * about this question.
+         */
         int lastAnswer = 0;
         List<Integer> answers = new ArrayList<>();
         List<List<Integer>> arr = new ArrayList<>();
