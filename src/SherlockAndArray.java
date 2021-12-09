878d2e02f9e4d95e33f4d0074d5b7ae3652a8b64
Brandon Lin <brandonlin9716@gmail.com>
2021-12-08 20:05:49 -0800

    Complete reflection for SherlockAndArray.java.

--- src/SherlockAndArray.java
+++ src/SherlockAndArray.java
@@ -7,6 +7,23 @@ public class SherlockAndArray {
      * sum of all elements to the left is equal to the sum of all elements to the right.
      */
     public static String balancedSums(List<Integer> arr) {
+        /*
+         * REFLECTION: Very similar to a previous problem I completed which also involved procedurally generating sums.
+         *             I want to say there's a more elegant way of coding the solution. I'll check the discussions
+         *
+         * After checking: I can move the if statement to the start of the loop to check the base case instead
+         *                 of having it at the bottom! In every iteration i, we are actually checking the validity of
+         *                 value i + 1! try to be flexible in how you use for loops to reduce code!
+         *
+         *                  for (int i = 0; i < arr.size(); i++) {
+                                if (leftSum == rightSum) {
+                                    return "YES";
+                                } else {
+                                    leftSum += arr.get(i);
+                                    rightSum -= arr.get(i + 1);
+                                }
+                            }
+         */
         int leftSum = 0;
         int rightSum = 0;
 
