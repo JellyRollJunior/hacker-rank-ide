8e6916f2ec8a4e95d19d8e045259956b514bdc1a
Brandon Lin <brandonlin9716@gmail.com>
2021-12-08 20:48:14 -0800

    Completed solution to RecursiveDigitSum.java.

---
+++ src/RecursiveDigitSum.java
@@ -0,0 +1,29 @@
+public class RecursiveDigitSum {
+
+    /*
+     * given an integer, we need to find the super digit of the integer.
+     * If x has only 1 digit, then its super digit is x.
+     * Otherwise, the super digit of x is equal to the super digit of the sum of the digits of x.
+     *
+     * find superDigit of n concatenated k times
+     */
+    public static int superDigit(String n, int k) {
+
+        if (n.length() == 1) {
+            return Integer.parseInt(n.repeat(k));
+        }
+
+        long sum = 0;
+        for (int i = 0; i < n.length(); i++) {
+            sum += Long.parseLong(String.valueOf(n.charAt(i)));
+        }
+        sum = sum * k;
+
+        return superDigit("" + sum, 1);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(superDigit("5009", 0));
+    }
+
+}
